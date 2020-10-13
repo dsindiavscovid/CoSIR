258e33f0801a25ab8b6fbc5947646ac41998b1a8
NayanaBannur <42090597+NayanaBannur@users.noreply.github.com>
2020-10-13 23:01:57 +0530

    Update streamlit page Remove navigation in sidebar Horizontal checkboxes Update titles

--- mainStreamLit.py
+++ mainStreamLit.py
@@ -1,75 +1,64 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-from solvers import *
 import altair as alt
-from matplotlib import cm
-from matplotlib.cm import ScalarMappable
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import pandas as pd
+import streamlit as st
 
-modes = ['About', 'Tool']
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", modes)
-if(selection == 'About'):
-    st.markdown("# We can write something about our work here")
-    
-if(selection == 'Tool'):
-#     graphs = ['SJIR', 'betaI', 'Phase']
-#     graphSelected = st.sidebar.radio("Display", graphs)
-    gamma = float(st.sidebar.slider('gamma', 0., 1., 0.1))
-    beta0 = float(st.sidebar.slider('beta0', 0., 5., 0.3))
-    t_end = int(st.sidebar.slider('T_end', 10, 1000, 300))
-    t = np.linspace(0, t_end-1, t_end)
-    I0 = int(st.sidebar.text_input("Init I", 200000))
-    R0 = int(st.sidebar.text_input("Init R", 200000))
-    N = int(st.sidebar.text_input("Population", 13000000))
-    r = float(st.sidebar.text_input("r", 3e-1))
-    e = float(st.sidebar.text_input("e", 2e-6))
-    S0 = N-I0-R0
+from solvers import *
 
-    if st.sidebar.checkbox('Control'):
-        eta = st.sidebar.slider('eta', 0., 20., 0.5)
-    else:
-        eta = 0
-    st.markdown("## Variables to plot")
-    toPlot = {}
-    toPlot['S'] = st.checkbox("S", value=True, key=None)
-    toPlot['I'] = st.checkbox("I", value=True, key=None)
-    toPlot['J'] = st.checkbox("J", value=True, key=None)
-    toPlot['R'] = st.checkbox("R", value=True, key=None)
-    toPlot['beta'] = st.checkbox("beta", value=False, key=None)
-    
-    S, I, R, beta = solve_sir_lv_control(S0, I0, R0, beta0, N, r, e, gamma, eta, t)
-    J = beta*S
-    variableValues = {"S":S, "I":I, "J":J, "R":R, "beta":beta}
-    graphDict = {}
-    for v in toPlot:
-        if(toPlot[v]):
-            graphDict[v] = variableValues[v]
-    
-    df = pd.DataFrame.from_dict(graphDict)
-    st.line_chart(df)
-    Istar = (r/e)
-    Jstar = gamma*N
-    phasePlotDict = {"J / JStar":J/Jstar, "I / Istar":I/Istar, "Time":t}
-    phasePlotDf = pd.DataFrame.from_dict(phasePlotDict)
-    
-    
-    scatter_chart = st.altair_chart(
-        alt.Chart(phasePlotDf)
-            .mark_circle(size=60)
-            .encode(x="J / JStar", y="I / Istar", color = alt.Color('Time', scale=alt.Scale(scheme='reds')))
-            .interactive()
-    )   
-        
-    
-    
-       
-#     if(graphSelected == 'SJIR'):
-#         SIR = {"S":S, "I":I, "R":R, "J":J}
-#         df = pd.DataFrame.from_dict(SIR)
-#         st.line_chart(df)
-#     elif(graphSelected == 'betaI'):
-#         betaI = {"beta":beta, "I":I}
-#         df = pd.DataFrame.from_dict(betaI)
-#         st.line_chart(df)
+st.sidebar.markdown('Parameters')
+gamma = float(st.sidebar.slider('gamma', 0., 1., 0.1))
+beta0 = float(st.sidebar.slider('beta0', 0., 5., 0.3))
+t_end = int(st.sidebar.slider('T_end', 10, 1000, 300))
+t = np.linspace(0, t_end - 1, t_end)
+I0 = int(st.sidebar.text_input("Init I", 200000))
+R0 = int(st.sidebar.text_input("Init R", 200000))
+N = int(st.sidebar.text_input("Population", 13000000))
+r = float(st.sidebar.text_input("r", 3e-1))
+e = float(st.sidebar.text_input("e", 2e-6))
+S0 = N - I0 - R0
+
+if st.sidebar.checkbox('Control'):
+    eta = st.sidebar.slider('eta', 0., 20., 0.5)
+else:
+    eta = 0
+
+# MAIN CONTENT
+
+st.header('CoSIR')
+
+# PLOT OF S-I-J-R-beta
+st.markdown('Select variables')
+var_plot_components = {
+    'variables': ['S', 'I', 'R', 'J', 'beta'],
+    'defaults': [True, True, True, True, False],
+    'key': [None] * 5
+}
+var_cols = st.beta_columns(5)
+to_plot = dict()
+for i in range(5):
+    to_plot[var_plot_components['variables'][i]] = var_cols[i].checkbox(var_plot_components['variables'][i],
+                                                                        value=var_plot_components['defaults'][i],
+                                                                        key=var_plot_components['key'][i])
+
+S, I, R, beta = solve_sir_lv_control(S0, I0, R0, beta0, N, r, e, gamma, eta, t)
+J = beta * S
+variableValues = {"S": S, "I": I, "J": J, "R": R, "beta": beta}
+graphDict = {}
+for v in to_plot:
+    if to_plot[v]:
+        graphDict[v] = variableValues[v]
+
+df = pd.DataFrame.from_dict(graphDict)
+st.markdown('### System Evolution')
+st.line_chart(df)
+Istar = (r / e)
+Jstar = gamma * N
+phasePlotDict = {"J / JStar": J / Jstar, "I / Istar": I / Istar, "Time": t}
+phasePlotDf = pd.DataFrame.from_dict(phasePlotDict)
+
+st.markdown('### Phase Plot')
+scatter_chart = st.altair_chart(
+    alt.Chart(phasePlotDf)
+        .mark_circle(size=60)
+        .encode(x="J / JStar", y="I / Istar", color=alt.Color('Time', scale=alt.Scale(scheme='reds')))
+        .properties(width=500, height=500)
+)
